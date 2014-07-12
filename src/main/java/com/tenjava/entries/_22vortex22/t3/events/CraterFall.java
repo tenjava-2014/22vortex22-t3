5a227ab245aede174851a14b9d238866f480cc3b
22vortex22 <harris.rothaermel@gmail.com>
2014-07-12 14:57:40 -0400

    plez work

---
+++ src/main/java/com/tenjava/entries/_22vortex22/t3/events/CraterFall.java
@@ -0,0 +1,5 @@
+package com.tenjava.entries._22vortex22.t3.events;
+
+public class CraterFall
+{
+}
--- src/main/java/com/tenjava/entries/_22vortex22/t3/events/DeathEvents.java
+++ src/main/java/com/tenjava/entries/_22vortex22/t3/events/DeathEvents.java
@@ -23,38 +23,25 @@ public class DeathEvents implements Listener
 	@EventHandler
 	public void onDeath(final PlayerDeathEvent e)
 	{
-<<<<<<< HEAD
-			Player p = e.getEntity();
-=======
-		//if(plugin.getConfig().getBoolean("options.deathevents"))
-		//{
-		Player p = e.getEntity();
+		final Player p = e.getEntity();
 		 if(p.hasPermission("VortexEvents.Death"))
 		 {
->>>>>>> 2cba030c3a5283c7d99c7720645c290fb48168fc
-			final Location loc = p.getLocation();
-			final World world = loc.getWorld();
 			new BukkitRunnable()
 			{
 			  public void run()
 			  {
-<<<<<<< HEAD
+				  Location loc = p.getLocation();
+				  World world = loc.getWorld();
+				  
 				   for (int i = 0; i < 10; i++)
-=======
-				  //plugin.getConfig().getInt("options.deatheffectquantity")
-				   for (int i = 0; i < 5; i++)
->>>>>>> 2cba030c3a5283c7d99c7720645c290fb48168fc
 				   {	 
 				   	world.strikeLightningEffect(loc);
 				   }
 			  }
 			}.runTaskLater(plugin, 5);
 			
-<<<<<<< HEAD
-=======
 		 }
-		//}
->>>>>>> 2cba030c3a5283c7d99c7720645c290fb48168fc
+		
 	}
 	
 	
